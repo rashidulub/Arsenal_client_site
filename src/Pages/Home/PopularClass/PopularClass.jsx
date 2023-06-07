fddc22daa69ef7ff7b79d0ca8c6a45857f46521a
rashidulub <rashidul016094@gmail.com>
2023-06-07 16:12:09 +0600

    popular instructor section card show

--- src/Pages/Home/PopularClass/PopularClass.jsx
+++ src/Pages/Home/PopularClass/PopularClass.jsx
@@ -16,7 +16,8 @@ const PopularClass = () => {
         <div>
             <div className="mx-auto text-center mt-5 lg:my-20 md:w-4/12 ">
                 <p className="text-orange-400 font-bold mb-2">---Visit This Section---</p>
-                <h3 className="text-3xl uppercase text-white  border-b-4 border-indigo-500 py-4">Popular Classes</h3>
+                <h3 className="text-3xl uppercase text-white  
+                border-b-4 border-indigo-500 py-4">Popular Classes</h3>
             </div>
            <div className="grid grid-cols-1 lg:px-10 lg:gap-5 lg:grid-cols-3 my-20 ">
            {
