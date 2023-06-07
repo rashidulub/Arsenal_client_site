7fcdd6cdfca7ae456e1b655910717aa0c838f568
rashidulub <rashidul016094@gmail.com>
2023-06-07 14:03:38 +0600

    popular instructor section added

--- src/Pages/Home/PopularClass/shared/ClassItem/ClassItem.jsx
+++ src/Pages/Home/PopularClass/shared/ClassItem/ClassItem.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 const ClassItem = ({item}) => {
-    const {name,image,instructor,enroll,rating , _id} =item
+    const {name,imageIns,instructor,enroll , _id} =item
     return (
         <div className="card card-compact w-96 bg-base-100 mb-10 shadow-xl">
-        <figure><img className='p-2 rounded h-72  ' src={image} alt="Shoes" /></figure>
+        <figure><img className='p-2 rounded h-72  ' src={imageIns} alt="Shoes" /></figure>
         <div className="card-body">
             <h2 className="card-title text-2xl text-black">Class Name: <span className='font-bold text-xl text-red-700'>{name}</span></h2>
             <p className="text-start  text-xl text-black">Enroll Class: <span className='font-bold text-xl text-red-700'>{enroll}</span> Students</p>
--- src/Pages/Home/PopularInstructors/PopularInstructors.jsx
+++ src/Pages/Home/PopularInstructors/PopularInstructors.jsx
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import InstructorsItem from '../InstructorsItem/InstructorsItem';
 
 const PopularInstructors = () => {
+    const [menu, setMenu] = useState([]);
+     useEffect(()=>{
+        fetch('menu.json')
+        .then(res=>res.json())
+        .then(data=> {
+            const PopularInstructor =data.filter(item=>item.category ==='popular_class');
+            setMenu(PopularInstructor)
+        })
+     }, [])
     return (
         <div>
-            
+             <div className="mx-auto text-center mt-5 lg:my-20 md:w-4/12 ">
+                <p className="text-orange-400 font-bold mb-2">---Visit Instructors Section---</p>
+                <h3 className="text-3xl uppercase text-white  border-b-4 border-indigo-500 py-4">Popular Instructors</h3>
+            </div>
+            <div className="grid grid-cols-1 lg:px-10 lg:gap-5 lg:grid-cols-3 my-20 ">
+           {
+                menu.map(item=> <InstructorsItem
+                    key={item._id}
+                    item={item}
+                ></InstructorsItem>)
+            }
+           </div>
         </div>
     );
 };
