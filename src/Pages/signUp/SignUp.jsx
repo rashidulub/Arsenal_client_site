91ff3a8ecfbe1f5278d13cedb5e3051e71589eb6
rashidulub <rashidul016094@gmail.com>
2023-06-08 16:40:50 +0600

    req page passwoerd validaation

--- src/Pages/signUp/SignUp.jsx
+++ src/Pages/signUp/SignUp.jsx
@@ -45,35 +45,46 @@ const SignUp = () => {
                     <img className='rounded-full' src={img} alt="" />
                 </div>
                 <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-                    <div className="card-body bg-green-600 rounded">
+                    <div className="card-body bg-green-300 rounded">
                         <h1 className="text-3xl text-center font-bold">Sign Up</h1>
-                        <form >
+                        <form onSubmit={handleSubmit(onSubmit)}>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Name</span>
                                 </label>
-                                <input type="text" name='name' placeholder="name" className="input input-bordered" />
+                                <input type="text" name='name'
+                                {...register("name",{ required: true })} placeholder="name" className="input input-bordered" />
+                                {errors.name && <span className='text-white bg-orange-600 text-center'>Name is required</span>}
                             </div>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Photo URL</span>
                                 </label>
-                                <input type="text" name='photo' placeholder="Photo URL" className="input input-bordered" />
+                                <input type="text" name='photo' {...register("photo",{ required: true })}placeholder="Photo URL" className="input input-bordered" />
+                                {errors.photo && <span className='text-white bg-orange-600 text-center'>Photo is required</span>}
                             </div>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Email</span>
                                 </label>
-                                <input type="text" name='email' placeholder="email" className="input input-bordered" />
+                                <input type="text" name='email' {...register("email",{ required: true })} placeholder="email" className="input input-bordered" />
+                                
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Password</span>
+                                </label>
+                                <input type="text" name='password' {...register("password",{ required: true })} placeholder="password" className="input input-bordered" />
+                                {errors.password && <span className='text-white bg-orange-600 text-center'>Password is required</span>}
+                               
                             </div>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Confirm Password</span>
                                 </label>
-                                <input type="text" name='password' placeholder="password" className="input input-bordered" />
-                                <label className="label">
-                                    <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-                                </label>
+                                <input type="text" name='password' {...register("password",{ required: true },{ required: true, maxLength: 20 })} placeholder="password" className="input input-bordered" />
+                                {errors.password && <span className='text-white bg-orange-600 text-center'>Password is required</span>}
+                               
                             </div>
                             <div className="form-control mt-6">
                                 <input className="btn btn-primary" type="submit" value="Sign Up" />
